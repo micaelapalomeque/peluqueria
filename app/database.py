84c3ca302570c7c037427b8a941d490aedbddb13
Micaela Palomeque <micaelapalomeque07@gmail.com>
2026-04-26 23:26:31 -0300

    "cambios de base de datos"

--- app/database.py
+++ app/database.py
@@ -1,8 +1,17 @@
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL =  os.getenv("DATABASE_URL")
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("No se encontro DATABASE_URL en el archivo .env")
 
 engine = create_engine(DATABASE_URL)
 
