48625d729e82ec19f50b204bb6f643f361398e2e
Micaela Palomeque <micaelapalomeque07@gmail.com>
2026-06-03 23:04:56 -0300

    cambios para vercel

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000"
+
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: BASE_URL,
 })
 
 export default api
