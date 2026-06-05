32a7a5049b5de97ba23da68956b519ef6aeb5d8b
Micaela Barbara Palomeque <133169148+micaelapalomeque@users.noreply.github.com>
2026-06-05 11:19:16 -0300

    actualizacion de CORS en main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # URL del frontend en desarrollo
+    allow_origins=[
+        "http://localhost:5173",
+        "https://peluqueria-isa.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
