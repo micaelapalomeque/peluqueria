dcfe4ba3eedc483e8edb68400bf11609db9e0ef8
Micaela Palomeque <micaelapalomeque07@gmail.com>
2026-06-08 20:41:34 -0300

    conexion bd

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,18 +6,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env")
+load_dotenv(BASE_DIR / ".env", override=False)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL is None:
     raise ValueError("No se encontro DATABASE_URL en el archivo .env")
 
-engine = create_engine(DATABASE_URL)
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 def get_db():
     db = SessionLocal()
     try:
